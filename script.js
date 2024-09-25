b1b6c41c0cf516c72e7c98e68cc111e7b6a66cf0
saiketramteke07 <161702056+saiketramteke07@users.noreply.github.com>
2024-09-25 20:16:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function scrollGallery(direction) {
     const gallery = document.getElementById('gallery-scroll');
-    const scrollAmount = 300; // Adjust this value to control scroll speed
+    const scrollAmount = 300;
     gallery.scrollBy({ 
         left: scrollAmount * direction, 
         behavior: 'smooth' 
